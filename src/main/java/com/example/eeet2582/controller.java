7998e39160233d128bce5a41eefaed94aa327b5a
quynguyn <s3878636@rmit.edu.vn>
2023-11-16 22:14:05 +0700

    add the controller

---
+++ src/main/java/com/example/eeet2582/controller.java
@@ -0,0 +1,14 @@
+package com.example.eeet2582;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controller {
+    
+    @GetMapping
+    String getHome(Model model){
+        return "homepage";
+    }
+}
