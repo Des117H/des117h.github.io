8900593fc6d80fbfd08f508e6c2e5b26780c2e85
quynguyn <s3878636@rmit.edu.vn>
2023-11-16 21:13:01 +0700

    website set up

---
+++ src/main/java/com/example/eeet2582/Eeet2582Application.java
@@ -0,0 +1,13 @@
+package com.example.eeet2582;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eeet2582Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eeet2582Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eeet2582/Eeet2582ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eeet2582;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eeet2582ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
