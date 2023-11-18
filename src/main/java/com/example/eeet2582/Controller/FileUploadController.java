5940de1daaeb1c0036a31c81dca96080df112e00
quynguyn <s3878636@rmit.edu.vn>
2023-11-19 00:38:30 +0700

    upload file tool

--- src/main/java/com/example/eeet2582/Controller/FileUploadController.java
+++ src/main/java/com/example/eeet2582/Controller/FileUploadController.java
@@ -1,35 +1,37 @@
 package com.example.eeet2582.Controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+import com.example.eeet2582.Service.FileStorageService;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/files")
 public class FileUploadController {
 
-    @RequestMapping("/")
-    public String uploadForm() {
-        return "upload-form";
-    }
+    @Autowired
+    private FileStorageService fileStorageService;
 
     @PostMapping("/upload")
-    @ResponseBody
-    public String handleFileUpload(@RequestParam("file") MultipartFile file) {
-        // Handle the file upload logic
-        if (!file.isEmpty()) {
-            try {
-                // Save the file to a storage location or process it
-                // For simplicity, we just print the file name here
-                System.out.println("Received file: " + file.getOriginalFilename());
-                return "File uploaded successfully!";
-            } catch (Exception e) {
-                return "Failed to upload file!";
-            }
-        } else {
-            return "Please select a file to upload.";
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            // Generate a unique file name or use the original file name
+            String fileName = "documents/" + System.currentTimeMillis() + "_" + file.getOriginalFilename();
+
+            // Upload the file to S3
+            fileStorageService.uploadFile(fileName, file);
+
+            // Return a success message or the S3 URL
+            return "File uploaded successfully! S3 URL: " + fileName;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Failed to upload file.";
         }
     }
 }
