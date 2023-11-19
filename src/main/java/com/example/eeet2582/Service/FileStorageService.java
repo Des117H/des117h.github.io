79a68cb273d97b9b5cf8383dbf6c4f0c64906a2c
Des117H <50516919+Des117H@users.noreply.github.com>
2023-11-19 14:17:28 +0700

    add dependence, edit src for homepage, css for upload document

--- src/main/java/com/example/eeet2582/Service/FileStorageService.java
+++ src/main/java/com/example/eeet2582/Service/FileStorageService.java
@@ -1,5 +1,27 @@
 package com.example.eeet2582.Service;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import lombok.Value;
+
 public class FileStorageService {
-    
+    @Autowired
+    private AmazonS3 amazonS3;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    public void uploadFile(String fileName, MultipartFile multipartFile) throws IOException {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(multipartFile.getSize());
+
+        amazonS3.putObject(new PutObjectRequest(bucketName, fileName, multipartFile.));
+    }
 }
