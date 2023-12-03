30f9928fd45363b60ae700bcff3396e936bef86b
quynguyn <s3878636@rmit.edu.vn>
2023-12-03 23:31:40 +0700

    working on get file

--- src/main/java/com/example/eeet2582/Service/FileService.java
+++ src/main/java/com/example/eeet2582/Service/FileService.java
@@ -8,10 +8,16 @@ import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteResult;
+import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+
+import org.apache.poi.extractor.ExtractorFactory;
+import org.apache.poi.extractor.POIOLE2TextExtractor;
+import org.apache.poi.extractor.POITextExtractor;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.io.ClassPathResource;
@@ -19,11 +25,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 import com.google.firebase.cloud.FirestoreClient;
+import com.google.firebase.cloud.StorageClient;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -65,10 +75,7 @@ public class FileService {
             return null;
     }
 
-    public void downloadFile(String urlString) {
-        String buckString = "architecture-grandma-bea3b.appspot.com";
-        storage.get();
-    }
+    
 
     // public User getUser(String userID) throws ExecutionException,
     // InterruptedException {
@@ -168,4 +175,44 @@ public class FileService {
         UUID uuid = UUID.randomUUID();
         return uuid.toString();
     }
+
+    public String extractTextFromDocx(String fileReferencePath) throws IOException {
+        // Storage storage = StorageOptions.newBuilder();
+        fileReferencePath = "0186e0bf-2e30-4d1d-b23b-f72bf7520fbc|1|2023-12-1_17:40:48|.docx";
+        Blob blob = storage.get("architecture-grandma-bea3b.appspot.com", fileReferencePath);
+        System.out.println(blob);
+        // InputStream inputStream = blob.reader();
+        // XWPFDocument document = new XWPFDocument(inputStream);
+        // XWPFWordExtractor extractor = new XWPFWordExtractor(document);
+        // String textContent = extractor.getText();
+        // extractor.close();
+        // document.close();
+        // return textContent;
+        return "run";
+    }
+
+    // public  void textProcess(){
+    //     String inputFile;
+    //     FileInputStream fis = new FileInputStream(inputFile);
+    //     POIFSFileSystem fileSystem = new POIFSFileSystem(fis);
+    //     // Firstly, get an extractor for the Workbook
+    //     POIOLE2TextExtractor oleTextExtractor = ExtractorFactory.createExtractor(fileSystem);
+    //     // Then a List of extractors for any embedded Excel, Word, PowerPoint
+    //     // or Visio objects embedded into it.
+    //     POITextExtractor[] embeddedExtractors =
+    //     ExtractorFactory.getEmbededDocsTextExtractors(oleTextExtractor);
+    //     for (POITextExtractor textExtractor : embeddedExtractors) {
+    //     // If the embedded object was an Excel spreadsheet.
+    //     if (textExtractor instanceof WordExtractor) {
+    //         WordExtractor wordExtractor = (WordExtractor) textExtractor;
+    //         String[] paragraphText = wordExtractor.getParagraphText();
+    //         for (String paragraph : paragraphText) {
+    //             System.out.println(paragraph);
+    //         }
+    //         // Display the document's header and footer text
+    //         System.out.println("Footer text: " + wordExtractor.getFooterText());
+    //         System.out.println("Header text: " + wordExtractor.getHeaderText());
+    //         }
+    //     }
+    // }
 }
---
+++ src/main/resources/static/js/edit.js
@@ -0,0 +1,15 @@
+
+
+console.log("quy nguyen")
+
+let toolbaroptions = [
+    [""]
+]
+
+let quill = new Quill("#editor",{
+    modules: {
+        toolbar: true,
+    },
+    theme: "snow"
+
+})
