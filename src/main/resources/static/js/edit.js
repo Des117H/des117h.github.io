883dd5c653f3a7bc57417a9c0ff995b8b7c0c3c2
quynguyn <s3878636@rmit.edu.vn>
2023-12-04 15:37:51 +0700

    nothing

--- src/main/resources/static/js/edit.js
+++ src/main/resources/static/js/edit.js
@@ -16,8 +16,7 @@ let fileID = "561f885c-d794-4985-b04d-b549eb993758";
 fetch('/document/get-metadata/' + fileID)
   .then(response => response.text())
   .then(data => {
-    // Access the Quill editor instance
-    console.log(data);
     quill.setText(data);
+ 
 });
 
