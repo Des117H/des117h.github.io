987809ffed88fed403e417eee36ab33a10bd9ca7
yuhneb1307 <s3878686@rmit.edu.vn>
2023-12-05 14:55:38 +0700

    add in function to toggle password

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -5,3 +5,17 @@ export const txtPassword = document.querySelector("txtPassword")
 export const btnLogin = document.querySelector("#btnLogin")
 export const btnSignUp = document.querySelector("#btnSignUp")
 export const btnLogOut = document.querySelector("#btnLogOut")
+
+let eyeicon = document.getElementById("eye-icon");
+let password = document.getElementById("txtPassword")
+eyeicon.onclick = function () {
+    if (password.type == "password") {
+        password.type = "text";
+        eyeicon.src = "/src/main/resources/static/image/eye-open.png"
+    }
+    else {
+        password.type = "password";
+        eyeicon.src = "/src/main/resources/static/image/eye-close.png"
+
+    }
+}
